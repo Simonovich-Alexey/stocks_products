82301dcfb9de2d37d361139277c4e6b8d0547c02
Alexey_Sim <simonovich_alexey@bk.ru>
2024-02-06 11:43:46 +0500

    Initial commit

---
+++ logistic/serializers.py
@@ -0,0 +1,61 @@
+from rest_framework import serializers
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        for position in positions:
+            print(position)
+            product = position.get('product')
+            quantity = position.get('quantity')
+            price = position.get('price')
+            StockProduct.objects.create(stock=stock, product=product,
+                                        quantity=quantity, price=price)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        for position in positions:
+            product = position.get('product')
+            quantity = position.get('quantity')
+            price = position.get('price')
+
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=product,
+                defaults={
+                    'quantity': quantity,
+                    'price': price,
+                },
+            )
+
+        return stock
